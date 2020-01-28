9c755ae684d5c393002c3c74aec15ba4891fefec
Roberto <robertopescee@hotmail.com>
2020-01-28 22:42:24 +0100

    make lists better in admin

--- innatura_crm/rufos/admin.py
+++ innatura_crm/rufos/admin.py
@@ -8,10 +8,16 @@ class PedidoProdutoInline(admin.StackedInline):
     extra = 2
 
 class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pedido_date', 'pedido_cliente', 'pedido_pagamento', 'pedido_status']
     inlines = [
         PedidoProdutoInline,
     ]
-
-admin.site.register(Cliente)
-admin.site.register(Produto)
 admin.site.register(Pedido, PedidoAdmin)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente_nome_fantasia', 'cliente_nome_contato', 'cliente_email', 'cliente_telefone', 'cliente_ativo']
+admin.site.register(Cliente, ClienteAdmin)
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'produto_nome', 'produto_codigo', 'produto_origem', 'produto_preco_tb1', 'produto_preco_tb2', 'produto_preco_tb3', 'produto_ativo']
+admin.site.register(Produto, ProdutoAdmin)
