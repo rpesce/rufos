01def14db9e360378188fc87ee5379c346e7fe9c
Roberto <robertopescee@hotmail.com>
2020-01-29 21:28:17 +0100

    add filters and search boxes

--- innatura_crm/rufos/admin.py
+++ innatura_crm/rufos/admin.py
@@ -9,6 +9,8 @@ class PedidoProdutoInline(admin.StackedInline):
 
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ['id', 'pedido_date', 'pedido_cliente', 'pedido_pagamento', 'pedido_status']
+    list_filter = ['pedido_cliente', 'pedido_status']
+    ordering = ('id', 'pedido_date', 'pedido_cliente', 'pedido_pagamento', 'pedido_status')
     inlines = [
         PedidoProdutoInline,
     ]
@@ -16,8 +18,14 @@ admin.site.register(Pedido, PedidoAdmin)
 
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ['id', 'cliente_nome_fantasia', 'cliente_nome_contato', 'cliente_email', 'cliente_telefone', 'cliente_ativo']
+    search_fields = ['cliente_nome_fantasia', 'cliente_nome_contato', 'cliente_email']
+    list_filter = ['cliente_nome_fantasia', 'cliente_nome_contato']
+    ordering = ('id', 'cliente_nome_fantasia', 'cliente_nome_contato', 'cliente_email', 'cliente_telefone', 'cliente_ativo')
 admin.site.register(Cliente, ClienteAdmin)
 
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ['id', 'produto_nome', 'produto_codigo', 'produto_origem', 'produto_preco_tb1', 'produto_preco_tb2', 'produto_preco_tb3', 'produto_ativo']
+    search_fields = ['produto_nome', 'produto_origem']
+    list_filter = ['produto_nome', 'produto_origem']
+    ordering = ('id', 'produto_nome', 'produto_codigo', 'produto_origem', 'produto_preco_tb1', 'produto_preco_tb2', 'produto_preco_tb3', 'produto_ativo')
 admin.site.register(Produto, ProdutoAdmin)
