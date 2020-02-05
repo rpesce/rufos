a5da0e569f1438e326cb38948e9f9caf417fbed8
Roberto <robertopescee@hotmail.com>
2020-02-05 23:48:48 +0100

    wip views

--- innatura_crm/rufos/urls.py
+++ innatura_crm/rufos/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #Ex. /rufos/
+    #Ex. /
     path('', views.index, name='index'),
-    #Ex. /rufos/3/
+    #Ex. /3/
     path('<int:cliente_id>/', views.detalhes_cliente, name='detalhes_cliente'),
-    #Ex. /rufos/3/pedidos/
+    #Ex. /3/pedidos/
     path('<int:cliente_id>/pedidos/', views.pedidos_cliente, name='pedidos_cliente')
 ]
--- innatura_crm/rufos/views.py
+++ innatura_crm/rufos/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Cliente
 
 
 def index(request):
-    return HttpResponse("Aqui o rufos vai colocar todo o front end (Criar uma API)")
+    #return HttpResponse("Aqui o rufos vai colocar todo o front end (Criar uma API)")
+    all_clients = Cliente.objects.all()
+    output = ', '.join([q.cliente_nome_fantasia for q in all_clients])
+    return HttpResponse(output)
 
 def detalhes_cliente(request, cliente_id):
     return HttpResponse("Você está olhando o cliente número: %s." % cliente_id)
