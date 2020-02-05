a5acf71c6a5dc9c5ff6107eac82c1d2cf370ab05
Roberto <robertopescee@hotmail.com>
2020-02-05 23:32:20 +0100

    wip first views

--- innatura_crm/rufos/urls.py
+++ innatura_crm/rufos/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Ex. /rufos/
     path('', views.index, name='index'),
+    #Ex. /rufos/3/
+    path('<int:cliente_id>/', views.detalhes_cliente, name='detalhes_cliente'),
+    #Ex. /rufos/3/pedidos/
+    path('<int:cliente_id>/pedidos/', views.pedidos_cliente, name='pedidos_cliente')
 ]
--- innatura_crm/rufos/views.py
+++ innatura_crm/rufos/views.py
@@ -7,3 +7,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Aqui o rufos vai colocar todo o front end (Criar uma API)")
+
+def detalhes_cliente(request, cliente_id):
+    return HttpResponse("Você está olhando o cliente número: %s." % cliente_id)
+
+def pedidos_cliente(request, cliente_id):
+    response = "Você está olhando os pedidos do cliente número: %s."
+    return HttpResponse(response % cliente_id)
